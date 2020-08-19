fd6337256f5ff8d26d80267f1c89f9e331c4e146
rdlucas2 <rdlucas2@gmail.com>
2020-08-19 02:26:06 -0400

    Update config.js

--- mydocs/docs/.vuepress/config.js
+++ mydocs/docs/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
      */
     description: description,
     
-    base: '/actionsHackathonSubmissions/'
+    base: '/actionsHackathonSubmissions/',
 
     /**
      * Extra tags to be injected to the page HTML `<head>`
