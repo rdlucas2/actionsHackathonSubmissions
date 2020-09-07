87f3a80067f7f564512dfc5cf304d4801bbf7dd2
Ryan Lucas <rdlucas2@gmail.com>
2020-09-07 14:40:26 -0400

    add all devices from file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 AZ_USERNAME=$1
 AZ_PASSWORD=$2
 AZ_TENANT=$3
-AZ_DEVICE_ID=$4
-AZ_HUB_NAME=$5
-DEVICE_LIST_FILE_PATH=$6
+#AZ_DEVICE_ID=$4
+AZ_HUB_NAME=$4
+DEVICE_LIST_FILE_PATH=$5
 
-#AZ_RG_NAME=$7
-#VM_ADMIN_USER=$8
-#VM_ADMIN_PASSWORD=$9
-#VM_NAME=$10
+#AZ_RG_NAME=$6
+#VM_ADMIN_USER=$7
+#VM_ADMIN_PASSWORD=$8
+#VM_NAME=$9
 
 #az --version
 
@@ -18,22 +18,23 @@ az extension add --name azure-iot
 
 az login --service-principal -u ${AZ_USERNAME} -p ${AZ_PASSWORD} --tenant ${AZ_TENANT}
 #az account show
-search_dir=$(pwd)
 
-for entry in "$search_dir"/*
-do
-  echo "$entry"
-done
+#search_dir=$(pwd)
+#for entry in "$search_dir"/*
+#do
+#  echo "$entry"
+#done
 
-echo ${DEVICE_LIST_FILE_PATH}
+cat ${DEVICE_LIST_FILE_PATH}
 
 while read -r line; 
 do 
-    echo $line; 
+    echo $line;
+    az iot hub device-identity create --device-id ${line} --hub-name ${AZ_HUB_NAME} --edge-enabled
 done < ${DEVICE_LIST_FILE_PATH}
 
 #create a new device
-az iot hub device-identity create --device-id ${AZ_DEVICE_ID} --hub-name ${AZ_HUB_NAME} --edge-enabled
+#az iot hub device-identity create --device-id ${AZ_DEVICE_ID} --hub-name ${AZ_HUB_NAME} --edge-enabled
 
 #get the device connection string
 #connString=$(az iot hub device-identity show-connection-string --device-id ${AZ_DEVICE_ID} --hub-name ${AZ_HUB_NAME})
